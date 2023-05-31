fa45a30d29aec6f1c7b99dfdc60f96099556b98a
Erzhan Kasymov <80050947+cazador18@users.noreply.github.com>
2023-05-31 17:55:20 +0600

    Add files via upload

---
+++ Swifty-Weather-master/MyWeather/CollectionViewCell/WeatherCollectionViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  WeatherCollectionViewCell.swift
+//  MyWeather
+//
+//  Created by Erzhan Kasymov on 31/5/23.
+//
+
+import UIKit
+
+class WeatherCollectionViewCell: UICollectionViewCell {
+
+    static let identifier = "WeatherCollectionViewCell"
+
+    static func nib() -> UINib {
+        return UINib(nibName: "WeatherCollectionViewCell",
+                     bundle: nil)
+    }
+
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var tempLabel: UILabel!
+
+    func configure(with model: HourlyWeatherEntry) {
+        self.tempLabel.text = "\(model.temperature)"
+        self.iconImageView.contentMode = .scaleAspectFit
+        self.iconImageView.image = UIImage(named: "clear")
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+}
